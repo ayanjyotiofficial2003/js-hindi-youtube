d85cb42d23618deff6df749c5d1cfbaf4358b9d8
AYANJYOTI MONDAL <133580983+ayanjyotiofficial2003@users.noreply.github.com>
2024-06-27 18:51:29 +0000

    discussion on variables

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -1,9 +1,20 @@
-const accountId = 144553
+const accountId = 144553 
 let accountEmail = "ayanjyoti@google.com"  
 var accountPassword = "12345678"
 accountCity = "Durgapur"
+let accountState;
 
+/*
+Prefer not to use var 
+Because of issue in block scope and functional scope
+*/
 
-accountId = 2
+// accountId = 2 // constant cannot be changed
+
+accountEmail = "ayan@yahoo.com"
+accountPassword = "5632145"
+accountCity = "Bengaluru"
 
 console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
