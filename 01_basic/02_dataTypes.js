d08d7f9b7151d52112e81613cf68846d90c3a073
AYANJYOTI MONDAL <133580983+ayanjyotiofficial2003@users.noreply.github.com>
2024-06-28 10:39:41 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all JS code as a newer version of JS code 
+
+// alert(3 + 3) // we are using nodejs, not browser, this code will only work in browser.
+
+// This code is not readable or this code is zero readability
+console.log(3
+    +
+    3)
+
+// This code is readable or this code is good readability
+console.log("Ayanjyoti");
+console.log(3+3);
+
+let name = "Ayanjyoti" // string datatypes
+let age = 18 // number datatypes
+let isLoggedIn = false // boolean datatypes
+
+// Primitive data types :-
+
+// number => 2^53 is the range of number
+// bigint
+// string => " abcd "
+// boolean => true/false
+// null => standalone value (Representation of an empty value) . Eg :- let city = null;
+// undefined => variable declared, but value not defined . Eg :- let state; 
+// symbol => unique
+
+// object data types :-
+
+console.log(typeof "Ayanjyoti"); // output => string data types
+console.log(typeof age); // output => number data types
+console.log(typeof null); // output => object data types
+console.log(typeof undefined); // output => undefined data types
