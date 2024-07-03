c68997e8e36d26c1b4ccbf39984120d67fb23875
AYANJYOTI MONDAL <133580983+ayanjyotiofficial2003@users.noreply.github.com>
2024-07-03 19:24:31 +0000

    datatype summary

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,55 @@
+// Primitive datatypes => call by value
+
+// 7 types :-
+// String, Number, BigInt, Boolean, Null, undefined, Symbol
+
+const score = 100 // Number data type
+
+const scoreValue = 100.3 // Number data type
+
+const isLoggedIn = false // Boolean data type
+
+const outSideTemp = null // Null data type
+
+let userEmail; // undefined data type
+
+// symbol data type
+const id = Symbol('123')
+const anotherId = Symbol('123')
+/*NOTE :- IN id and anotherId the given value is same but the return value is not same,
+this is the work of SYMBOL data type */
+// console.log(id === anotherId); // output => false
+
+const bigNumber = 3465688954751225321255n  // BigInt data type
+
+
+
+// Refrenced datatypes (Non-primitive data types) => call by refrences
+
+// Array, Objects, Functions
+
+// Array data type
+const heros = ["shaktiman", "naagraj", "doga"];
+
+// Object data type
+let myObj = {
+    name: "Ayanjyoti",
+    age: 18,
+}
+
+// Function data type
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// typeof => it return the data type of a particular variable
+console.log(typeof bigNumber); // output => bigint
+console.log(typeof myFunction); // output => function
+console.log(typeof outSideTemp); // output => object
+console.log(typeof scoreValue); // output => number
+console.log(typeof heros); // output => object
+console.log(typeof myObj); // output => object
+console.log(typeof anotherId); // output => symbol
+console.log(typeof id); // output => symbol
+
+// NOTE :- JavaScripts is a dynamically typed language
