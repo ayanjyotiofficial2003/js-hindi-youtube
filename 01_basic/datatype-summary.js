e49b845b0188c295f68a51314bc71c6bc26a517d
AYANJYOTI MONDAL <133580983+ayanjyotiofficial2003@users.noreply.github.com>
2024-07-04 10:44:09 +0000

    datatype summary

--- 01_basic/datatype-summary.js
+++ 01_basic/datatype-summary.js
@@ -43,13 +43,44 @@ const myFunction = function(){
 }
 
 // typeof => it return the data type of a particular variable
-console.log(typeof bigNumber); // output => bigint
-console.log(typeof myFunction); // output => function
-console.log(typeof outSideTemp); // output => object
-console.log(typeof scoreValue); // output => number
-console.log(typeof heros); // output => object
-console.log(typeof myObj); // output => object
-console.log(typeof anotherId); // output => symbol
-console.log(typeof id); // output => symbol
+// console.log(typeof bigNumber); // output => bigint
+// console.log(typeof myFunction); // output => function
+// console.log(typeof outSideTemp); // output => object
+// console.log(typeof scoreValue); // output => number
+// console.log(typeof heros); // output => object
+// console.log(typeof myObj); // output => object
+// console.log(typeof anotherId); // output => symbol
+// console.log(typeof id); // output => symbol
 
 // NOTE :- JavaScripts is a dynamically typed language
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Memory :- 1) Stack , 2) Heap
+
+// 1) Stack (used by primitive data types) , 2) Heap (used by Non - primitive data types)
+
+
+// STACK => used by primitive data types => call by value
+let myYoutubeChannel = "ayanjyotimondaldotcom"
+
+let anotherName = myYoutubeChannel
+anotherName = "Ayanjyotidotcom"  
+
+console.log(anotherName); // output => Ayanjyotidotcom
+console.log(myYoutubeChannel); // output => ayanjyotimondaldotcom
+
+
+// HEAP => used by Non - primitive data types => call by refrences
+
+let userOne = {
+    email: "user@google.com", 
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "ayanjyoti@google.com"
+
+console.log(userOne.email); // output => ayanjyoti@google.com
+console.log(userTwo.email); // output => ayanjyoti@google.com
