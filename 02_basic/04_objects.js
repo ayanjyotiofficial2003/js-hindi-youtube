8eac6290a280789d2a3c395c971fd378113ae77c
AYANJYOTI MONDAL <133580983+ayanjyotiofficial2003@users.noreply.github.com>
2024-07-11 10:40:02 +0000

    object and json talks

--- 02_basic/04_objects.js
+++ 02_basic/04_objects.js
@@ -30,5 +30,19 @@ const regularUser = {
 
 // console.log(regularUser.fullname.userfullname); // output => { firstname: 'Ayanjyoti', lastname: 'Mondal' }
 
-console.log(regularUser.fullname.userfullname.firstname); // output => Ayanjyoti
+// console.log(regularUser.fullname.userfullname.firstname); // output => Ayanjyoti
+
+
+const obj1 = {1:"a", 2:"b"}
+const obj2 = {3:"a", 4:"b"}
+const obj4 = {5:"a", 6:"b"}
+
+// const obj3 = {obj1, obj2} // output => { obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'a', '4': 'b' } }
+
+// const obj3 = Object.assign(obj1, obj2) // output => { '1': 'a', '2': 'b', '3': 'a', '4': 'b' }
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // output => { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+const obj3 = {...obj1, ...obj2, ...obj4} // output => { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+console.log(obj3);
 
