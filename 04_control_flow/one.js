774e0c77ffc3c5108c00e38171908f554a03a787
AYANJYOTI MONDAL <133580983+ayanjyotiofficial2003@users.noreply.github.com>
2024-07-15 15:52:47 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,76 @@
+// conditional statement 
+
+// if statements
+
+const isUserLoggedIn = true
+
+// if(isUserLoggedIn){
+//     console.log("executed");
+// }
+
+// if(3 != 2){
+//     console.log("executed");
+// }
+
+// if(2 == "2"){
+//     console.log("executed");
+// }
+
+// if(2 === 2){
+//     console.log("executed");
+// }
+
+// if(2 !== "2"){
+//     console.log("executed");
+// }
+
+
+// checking opearators 
+// <, >, <=, >=, !=, ==, ===(strict equal operator) , !==(strict not equal operator) 
+
+
+
+// const temperature = 41
+// if(temperature < 50){
+//     console.log("Temperature is below 50");
+// }
+// else{
+//     console.log("Temperature is not below 50");
+// }
+
+// console.log("Executed");
+
+
+
+// const score = 200
+
+// if (score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`); // output => error (power is not defined)
+
+
+
+// const balance = 1000
+// This is short hand notation of if statements
+// if (balance > 500) console.log("Yes, balance is >500");
+
+// This code will run, but this code is not readable, and writting this code is not good practise
+// if (balance > 500) console.log("Yes, balance is >500"), console.log("test2");
+
+// const balance = 1500;
+
+// if (balance < 500) {
+//     console.log("Balance is less than 500");
+// }
+// else if (balance < 750) {
+//     console.log("Balance is less than 750");
+// }
+// else if (balance < 900) {
+//     console.log("Balance is less than 900");
+// }
+// else{
+//     console.log("Balance is less than 1500");
+// }
